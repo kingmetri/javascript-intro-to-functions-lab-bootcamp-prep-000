b51f41b242cda187d2ff2606b3a2a4c9b58c8bb1
kingmetri <demetribethel@gmail.com>
2017-12-22 23:46:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string) {
+return 'hello'.toUpperCase()
+}
+
+function whisper(string){
+  return 'HELLO'.toLowerCase()
+}
+
+function logShout(log){
+  console.log('log').toUpperCase()
+}
